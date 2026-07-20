584d21c7ec48bde832a977e04dd979e951eaa133
schwissig <schwissig@tutanota.com>
2026-07-19 22:52:17 -0500

    Add user interface

--- src/main/groovy/net/schwissig/Main.groovy
+++ src/main/groovy/net/schwissig/Main.groovy
@@ -1,19 +1,10 @@
 package net.schwissig
 
+import net.schwissig.ui.CounterSheetConverterUI
+
 class Main {
 
     static void main(String[] args) {
-        if (args.length != 2) {
-            println "Error: Not enough arguments."
-            println "Usage:"
-            println "   java -jar counter-sheet-converter.jar <counter-sheet-data.json> <output-directory>"
-            return
-        }
-
-        try {
-            CounterSheetConverter.convert(args[0], args[1])
-        } catch (Exception e) {
-            println "Error: ${e.getMessage()}"
-        }
+        new CounterSheetConverterUI()
     }
 }
